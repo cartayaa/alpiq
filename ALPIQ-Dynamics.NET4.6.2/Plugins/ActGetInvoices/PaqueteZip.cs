ae085e6a75707ad07abf6fba515ccc3fbb26554c
Andrés Cartaya <andres.cartaya@atos.net>
2022-09-05 12:07:15 +0200

    Fecha PO Contrato | 05-09-2022 | Fecha Ordenes de Contrato

---
+++ ALPIQ-Dynamics.NET4.6.2/Plugins/ActGetInvoices/PaqueteZip.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Packaging;
+using System.IO;
+
+namespace ActGetInvoices
+{
+    class PaqueteZip
+    {
+        private FileStream zipStream;
+        private Package paquete;
+
+        public PaqueteZip(String nombreZip)
+        {
+            zipStream = File.Open(nombreZip, FileMode.Create);
+            paquete = ZipPackage.Open(zipStream, FileMode.Create);
+            
+        }
+
+
+        public void AddFile(Stream stream, string Name)
+        {
+            Uri partUriDocument = PackUriHelper.CreatePartUri(new Uri(Name, UriKind.Relative));
+            PackagePart packagePartDocument = paquete.CreatePart(partUriDocument, "application/pdf", CompressionOption.Normal);
+            CopyStream(stream, packagePartDocument.GetStream());
+            stream.Close();
+        }
+
+
+        public void AddFile(String fichero, string Name)
+        {
+            AddFile(File.OpenRead(fichero), Name);
+        }
+
+        public void Close()
+        {
+            //paquete.DeletePart(PackUriHelper.CreatePartUri(new Uri(@"/[Content_Types].xml", UriKind.Relative)));
+            paquete.Close();
+            zipStream.Close();
+        }
+
+
+        private static void CopyStream(Stream source, Stream target)
+        {
+            const int bufSize = 0x1000;
+            byte[] buf = new byte[bufSize];
+            int bytesRead = 0;
+            while ((bytesRead = source.Read(buf, 0, bufSize)) > 0)
+                target.Write(buf, 0, bytesRead);
+        }  
+    }
+}
---
+++ ALPIQ-Dynamics.NET4.6.2/Plugins/ActGetInvoices/ScriptBorradoTemporales.ps1
@@ -0,0 +1,11 @@
+﻿Param(
+[Parameter(Mandatory=$True,Position=1)]
+[string]$directorio
+)
+$mascara = "*.txt, *.rar"
+$numMins = 60
+$curDate = Get-Date
+$fechaLimite = $curDate.AddMinutes(-$numMins)
+$archivosABorrar = Get-ChildItem  $directorio -Include $mascara 
+
+Get-ChildItem -Path $directorio -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $fechaLimite } | Remove-Item -Force
